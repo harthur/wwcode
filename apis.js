4134e2fc71a409f7e59919eb5029409f4d2feddf
Heather Arthur <fayearthur@gmail.com>
2012-02-28 16:51:02 -0800

    initial code

---
+++ apis.js
@@ -0,0 +1,20 @@
+function getMeetupStuff(method, params, callback) {
+  var url = "http://api.meetup.com/2/" + method + "/?callback=?";
+  params = $.extend({
+    key: "29363e123f3a52532a7b306b95b4d",
+    group_urlname: "Women-Who-Code-SF"
+  }, params);
+
+  $.getJSON(url, params, callback);
+}
+
+function searchTwitter(term, callback) {
+  var url = "http://search.twitter.com/search.json?callback=?";
+  var params = {
+    q: term,
+    rpp: 5,
+    result_type: "recent"
+  };
+
+  $.getJSON(url, params, callback);
+}
